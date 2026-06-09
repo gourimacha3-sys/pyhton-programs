6c2dc1869b66edef114b34468f926ae2d5e74b44
Gouri <gourimacha3@gmail.com>
2026-06-08 21:32:54 -0900

    Delete comparision2.py

--- comparision2.py
+++
@@ -1,8 +0,0 @@
-str1="gouri"
-str2="sruthi"
-print("equal to:",str1==str2)
-print("not equal:",str1!=str2)
-print("greater than:",str1>str2)
-print("less than:",str1<str2)
-print("greater than equals:",str1>=str2)
-print("less than equals:",str1<=str2)
