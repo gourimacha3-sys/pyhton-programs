339f460cebce9137926dd5fb73e86a5fd31e2197
Gouri <gourimacha3@gmail.com>
2026-06-10 19:12:51 +0530

    Delete program 4.3.py

--- program 4.3.py
+++
@@ -1,9 +0,0 @@
-def factorial(n):
-    if n == 0:
-        return 1
-    else:
-        return n* factorial(n-1)
-for i in range(1,11):
-    result=factorial(i)
-    print("factorial of",i,"is",result)
-        
