f0c5a33ea3fcd6d2254d03ea1d4b85e5c7838b2c
Gouri <gourimacha3@gmail.com>
2026-06-15 20:04:00 -1000

    Delete rectangle.py

--- rectangle.py
+++
@@ -1,6 +0,0 @@
-l=float(input("enter the length:"))
-b=float(input("enter the breadth:"))
-area=l*b
-perimeter=2*(l+b)
-print("the area of rectangle is:",area)
-print("the perimeter of rectangle is:",perimeter)
